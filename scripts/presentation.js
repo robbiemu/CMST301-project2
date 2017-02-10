1f0ba3ee0a0b25c6d76bff8e47fae811404f9bfe
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-02-10 17:48:35 -0500

    cleaned up load order logic

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -25,45 +25,23 @@ function template_callback () {
 }
 
 // template numbers in series
-let series = [...Array(15)].map((x,i) => i)
+let series = [...Array(15)].map((x,i) => {
+  $('section.series').after(`<div id="ele${14-i}">{{ ele }}</div>`)
+  return i
+})
 
-const el = 'section.series > div[template-for]'
-const template = $(el)
-
-const forTemplate = $(el).attr('template-for').split(' in ', 2)
-const item = forTemplate[0]
-
-const promises = series.map((ele) => (
+const promises = series.map(i => (
   $.ajax({
-    url: `templates/ele${ele}.template.html`,
+    url: `templates/ele${i}.template.html`,
   }).then((data) => {
-    console.log('Processing ele' + ele)
-
-    const $copyEl = $(el).clone()
-    const r = new RegExp('\\{\\{\\s*' + item + '\\s*\\}\\}', 'g')
-    const html = $copyEl.html().replace(r, data)
-
-    $(html).insertBefore('section.series')
+    console.log('Processing ele' + i)
+    $('#ele'+i).replaceWith(data)
   }, (e) => {
     console.error(e)
   })
 ))
 
-Promise.series = (promiseArr) => {
-  return Promise.reduce(promiseArr, async (values, promise) => {
-    if(promise === undefined) {
-      console.log('processing undefined promise', promiseArr) 
-      return
-    }
-    console.log(promise.toString())
-    return await promise().then((result) => {
-      values.push(result);
-      return values;
-    });
-  }, []);
-};
-
-Promise.series(promises).then(() => {
+Promise.all(promises).then(() => {
   $('section.series').detach()
   template_callback()
 })
