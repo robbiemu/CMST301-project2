f301ddf59c350b493dccfb9fd61e92f65780fb2a
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-02-10 16:26:03 -0500

    prefinal.2

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -1,15 +1,5 @@
 $(function() {
-/*  document.addEventListener('impress:stepenter', function (e) {
-  switch (e.action) {
-    case 'next':
-    case 'prev':
-      console.log(e.action)
-      break
-    case 'goto':
-      console.log('going to ' + e.step)
-  }
-}, false); */
-
+  
 function template_callback () {
   $('.oEmbed').each((i,el) => {
     const url = 'http://twitter.com' + $(el).data('path')
@@ -35,33 +25,52 @@ function template_callback () {
 }
 
 const series = { series1:[0,1,2,3,4,5,6], series2:[7,8,9,10], series3:[11,12,13,14] }
+Object.keys(series).forEach(s => {
+  const template = $('#' + s).find('div[template-for]')
+  const outer_last = template.length
 
-const outer_last = $('section.series').find('div[template-for]').length
-$('section.series').find('div[template-for]').each((i, el) => {
-  const parent = $(el).parent()
-  const forTemplate = $(el).attr('template-for').split(' in ', 2)
-  const item = forTemplate[0], 
-        group = forTemplate[1]
+  template.each((i, el) => {
+    const parent = $(el).parent()
+    const forTemplate = $(el).attr('template-for').split(' in ', 2)
+    const item = forTemplate[0], 
+          group = forTemplate[1]
 
-  const inner_last = series[group].length
+    const inner_last = series[group].length
 
-const promises = series[group].map((ele) => (
-    $.ajax({
+    const promises = series[group].map((ele) => (
+      $.ajax({
         url: `templates/ele${ele}.template.html`,
-    }).then((data) => {
+      }).then((data) => {
+        console.log('Processing ele' + ele)
+
         const $copyEl = $(el).clone()
         const r = new RegExp('\\{\\{\\s*' + item + '\\s*\\}\\}', 'g')
         const html = $copyEl.html().replace(r, data)
 
         $(html).insertBefore(parent)
-    }, (e) => {
+      }, (e) => {
         console.error(e)
-    })
-  ))
+      })
+    ))
 
-  Promise.all(promises).then(() => {
-    $(parent).detach()
-    template_callback()
+    Promise.series = (promiseArr) => {
+      return Promise.reduce(promiseArr, (values, promise) => {
+        if(promise === undefined) {
+          console.log('processing undefined promise') 
+        }
+          return
+        console.log(promise.toString())
+        return promise().then((result) => {
+          values.push(result);
+          return values;
+        });
+      }, []);
+    };
+
+    Promise.series(promises).then(() => {
+      $(parent).detach()
+      template_callback()
+    })
   })
 })
 
